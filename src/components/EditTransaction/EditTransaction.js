924a1e56e914bdbed1c89048b54e95836cfda7e8
Gene Knight <GeneKnight4@GMail.com>
2018-04-18 17:03:53 -0700

    add src

---
+++ src/components/EditTransaction/EditTransaction.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class EditTransaction extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default EditTransaction;
---
+++ src/components/ParentHome/ParentHome.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ParentHome extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ParentHome;
